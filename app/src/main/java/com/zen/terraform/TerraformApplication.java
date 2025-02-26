4bf2b58fb991e9ec0ed07d07c413b2a21ac5d964
Melvin Tivane <melvinshuster47@gmail.com>
2025-02-26 18:50:57 +0200

    Initial commit

---
+++ app/src/main/java/com/zen/terraform/TerraformApplication.java
@@ -0,0 +1,13 @@
+package com.zen.terraform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraformApplication.class, args);
+	}
+
+}
